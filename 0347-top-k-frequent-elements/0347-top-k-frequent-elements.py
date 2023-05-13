8fbc928efdb474b161b4113b23107264106cf5d2
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-05-12 21:05:41 -0700

    Time: 87 ms (46.00%), Space: 20.6 MB (29.52%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,8 +6,9 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        # approach: dict to store num frequency, then sort dict
+        # approach 1: dict to store num frequency, then sort dict
         
+        '''
         if len(nums) == 1:
             return nums
         
@@ -27,3 +28,39 @@ class Solution(object):
             res.append(item[0])
 
         return res
+        '''
+        
+        # approach 2: dict to store num frequency, array of frequency -> nums
+        
+        if len(nums) == 1:
+            return nums
+        
+        d = {}
+        res = []
+        
+        for num in nums:
+            if num in d:
+                d[num] += 1
+            else:
+                d[num] = 1
+        
+        arr = [[] for i in range(len(nums) + 1)]
+        
+        for num in d:
+            freq = d[num]
+            arr[freq].append(num)
+        
+        for i in reversed(range(len(arr))):
+            for num in arr[i]:
+                res.append(num)
+                
+                if len(res) == k:
+                    return res
+        
+        
+        
+        
+        
+        
+        
+        
