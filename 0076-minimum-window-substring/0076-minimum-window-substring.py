8400e68a3fac68ec9c28309b27be0b7c0ec97e19
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-05-16 15:42:49 -0700

    Time: 463 ms (17.44%), Space: 14 MB (98.76%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -6,48 +6,43 @@ class Solution(object):
         :rtype: str
         """
         
-        def isValidWindow(sd, td):
-            for char in td:
-                if sd[char] < td[char]:
-                    return False
-            
-            return True
-        
         if len(s) < len(t): 
             return ''
         
-        td = {}
-        sd = {}
+        td, sd = {}, {}
         res = ''
         init = False
         
         for char in t:
             td[char] = 1 + td.get(char, 0)
             sd[char] = 0
-            
-        print(td, sd)
+        
+        have, need = 0, len(td)
         
         l = 0
-        for r in range(len(s)):
-            char = s[r]
-            
-            if char in td:
+        for r, char in enumerate(s):
+            if char in sd:
                 sd[char] += 1
+                
+                if sd[char] == td[char]:
+                    have += 1
             
-            while isValidWindow(sd, td):
+            while have == need:
                 if not init:
                     res = (s[l : r + 1])
                     init = True
                     
                 if len(s[l : r + 1]) < len(res):
                     res = (s[l : r + 1])
-                    
+                
                 if s[l] in sd:
                     sd[s[l]] -= 1
                     
-                l += 1
+                    if sd[s[l]] < td[s[l]]:
+                        have -= 1
+                    l += 1
                 
-                while l < r and s[l] not in td:
+                while l < r and s[l] not in sd:
                     l += 1
         return res
         
