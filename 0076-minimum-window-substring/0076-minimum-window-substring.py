9e5c0ee317163363755e9f1b60a35d67b1c47422
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-05-16 15:56:36 -0700

    Time: 507 ms (16.75%), Space: 14 MB (91.90%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -18,35 +18,30 @@ class Solution(object):
         
         for char in t:
             td[char] = 1 + td.get(char, 0)
-            sd[char] = 0
         
         have, need = 0, len(td)
         
         l = 0
         for r, char in enumerate(s):
-            if char in sd:
-                sd[char] += 1
+            sd[char] = 1 + sd.get(char, 0)
                 
-                if sd[char] == td[char]:
-                    have += 1
+            if char in td and sd[char] == td[char]:
+                have += 1
             
             while have == need:
                 if not init:
-                    res = (s[l : r + 1])
+                    res = s[l : r + 1]
                     init = True
                     
                 if len(s[l : r + 1]) < len(res):
-                    res = (s[l : r + 1])
+                    res = s[l : r + 1]
                 
-                if s[l] in sd:
-                    sd[s[l]] -= 1
+                sd[s[l]] -= 1
                     
-                    if sd[s[l]] < td[s[l]]:
-                        have -= 1
-                    l += 1
-                
-                while l < r and s[l] not in sd:
-                    l += 1
+                if s[l] in td and sd[s[l]] < td[s[l]]:
+                    have -= 1
+                    
+                l += 1
         return res
         
         
