8e0d973e21ce71786a7e03b6924ac08d54c414c1
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-05-15 23:30:38 -0700

    Time: 98 ms (42.45%), Space: 14.9 MB (72.58%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -5,10 +5,7 @@ class Solution(object):
         :rtype: int
         """
         
-        # approach: move pointer along x-axis while keeping track of "holes", i.e.
-        # spaces from shorter bars. move pointer until bar of >= height is found,
-        # then add the area of all of the holes. do the same thing with a right
-        # pointer
+        # approach: two pointers and store leftMax and rightMax
         
         l, r = 0, len(height) - 1
         leftMax, rightMax = height[l], height[r]
