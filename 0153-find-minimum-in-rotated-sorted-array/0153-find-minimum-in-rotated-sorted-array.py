ec8d2df39e243c9a2f2064665e816e05319ef4a0
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-05-23 23:34:38 -0700

    Time: 54 ms (32.65%), Space: 16.7 MB (15.64%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,24 +1,24 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         
-        # approach: binary search to find inflection point,
-        # i.e. nums[i] < nums[i - 1]
-        
-        if len(nums) == 1:
-            return nums[0]
-        
+        # approach: binary search while keeping track of min
+    
         l, r = 0, len(nums) - 1
         res = float(inf)
         
         while l <= r:
+            if nums[l] <= nums[r]:
+                res = min(res, nums[l])
+                break
+                
             m = (l + r) // 2
             res = min(res, nums[m])
             
-            if nums[m] < nums[r]:
-                r = m - 1
-            elif nums[m] > nums[r]:
+            if nums[m] >= nums[l]:
                 l = m + 1
             else:
-                break
-            
+                r = m - 1
+        
         return res
+
+    
