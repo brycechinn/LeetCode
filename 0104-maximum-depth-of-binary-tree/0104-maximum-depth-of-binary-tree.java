8f88a6a47ea64b7bf1905028a2edf2214250a37c
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-05-28 02:06:33 -0700

    Time: 1 ms (11.18%), Space: 43.8 MB (8.97%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     // approach 1: recursive DFS
-    
+    /*
     public int maxDepth(TreeNode root) {
         return DFS(root);
     }
@@ -26,5 +26,37 @@ class Solution {
         }
         
         return 1 + Math.max(DFS(node.left), DFS(node.right));
+    }
+    */
+    
+    // approach 2: BFS
+    
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int level = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.remove();
+                
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            
+            level++;
+        }
+        
+        return level;
     }
 }
