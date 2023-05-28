0f42bdc8d118540105196e33ffa3296ca0e0ca4f
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-05-27 19:22:11 -0700

    Time: 2 ms (89.69%), Space: 41 MB (15.80%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> hashmap = new HashMap<>();
+        
+        hashmap.put('(', ')');
+        hashmap.put('{', '}');
+        hashmap.put('[', ']');
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (hashmap.containsKey(c)) {
+                stack.add(c);
+            } else {
+                if (!stack.empty() && c == hashmap.get(stack.peek())) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.empty();
+    }
+}
