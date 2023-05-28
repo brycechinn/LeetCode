10e7658454a5e33122a27cb0be645f47999a8ad5
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-05-27 18:26:21 -0700

    Time: 17 ms (26.60%), Space: 43.8 MB (8.40%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         // approach 1: sort strings then compare
         
+        /*
         char sChars[] = s.toCharArray();
         char tChars[] = t.toCharArray();
         
@@ -9,5 +10,29 @@ class Solution {
         Arrays.sort(tChars);
         
         return Arrays.equals(sChars, tChars);
+        */
+        
+        // approach 2: add chars to hashmap then compare
+        
+        Map<Character, Integer> sHash = new HashMap<>();
+        Map<Character, Integer> tHash = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            if (sHash.containsKey(c)) {
+                sHash.put(c, sHash.get(c) + 1);
+            } else {
+                sHash.put(c, 1);
+            }
+        }
+        
+        for (char c : t.toCharArray()) {
+            if (tHash.containsKey(c)) {
+                tHash.put(c, tHash.get(c) + 1);
+            } else {
+                tHash.put(c, 1);
+            }
+        }
+        
+        return sHash.equals(tHash);
     }
 }
