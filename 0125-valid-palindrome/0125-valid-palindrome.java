b5a426e71be753b461cecb56ed0aed79ebae4c8b
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-06-14 13:51:57 -0700

    Time: 4 ms (70.12%), Space: 42.1 MB (86.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,23 +1,22 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // approach: two pointers, compare l and r
-        
         int l = 0;
         int r = s.length() - 1;
         
-        while (l <= r) {
-            while (l < s.length() && !Character.isLetter(s.charAt(l)) && 
-                   !Character.isDigit(s.charAt(l))) {
+        while (l < r) {
+            while (l < s.length() && !isAlphanumeric(s.charAt(l))) {
                 l++;
             }
             
-            while (r >= 0 && !Character.isLetter(s.charAt(r)) && 
-                   !Character.isDigit(s.charAt(r))) {
+            while (r >= 0 && !isAlphanumeric(s.charAt(r))) {
                 r--;
             }
             
-            if (l < s.length() && r >= 0 && Character.toLowerCase(s.charAt(l)) != 
-                Character.toLowerCase(s.charAt(r))) {
+            if (l >= r) {
+                break;
+            }
+            
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
                 return false;
             }
             
@@ -26,5 +25,9 @@ class Solution {
         }
         
         return true;
+    }
+    
+    private boolean isAlphanumeric(char c) {
+        return Character.isLetter(c) || Character.isDigit(c); 
     }
 }
