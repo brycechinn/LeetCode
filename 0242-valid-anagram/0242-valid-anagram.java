ddfe59614f56171c5f87ee9493fb8f0820ea2035
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-06-14 13:20:47 -0700

    Time: 8 ms (35.28%), Space: 42.1 MB (88.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,50 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // approach 1: sort strings then compare
-        
-        /*
-        char sChars[] = s.toCharArray();
-        char tChars[] = t.toCharArray();
-        
-        Arrays.sort(sChars);
-        Arrays.sort(tChars);
-        
-        return Arrays.equals(sChars, tChars);
-        */
-        
-        // approach 2: add chars to hashmap then compare
-        
-        /*
-        Map<Character, Integer> sHash = new HashMap<>();
-        Map<Character, Integer> tHash = new HashMap<>();
-        
-        for (char c : s.toCharArray()) {
-            if (sHash.containsKey(c)) {
-                sHash.put(c, sHash.get(c) + 1);
-            } else {
-                sHash.put(c, 1);
-            }
-        }
-        
-        for (char c : t.toCharArray()) {
-            if (tHash.containsKey(c)) {
-                tHash.put(c, tHash.get(c) + 1);
-            } else {
-                tHash.put(c, 1);
-            }
-        }
-        
-        return sHash.equals(tHash);
-        */
-        
-        // approach 3: add char counts to array store then compare
-        
-        s = s.toLowerCase();
-        t = t.toLowerCase();
+        // approach: two arrays to store char counts
         
         if (s.length() != t.length()) {
             return false;
         }
+        
+        s = s.toLowerCase();
+        t = t.toLowerCase();
         
         int[] countsS = new int[26];
         int[] countsT = new int[26];
