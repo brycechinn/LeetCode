3ad9954a992b148564c5655dfa565ac2a6c5eacb
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-06-16 15:13:48 -0700

    Time: 8 ms (74.03%), Space: 41.7 MB (86.94%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -2,6 +2,7 @@ class Solution {
     public int characterReplacement(String s, int k) {
         // approach: hashmap of char : frequency in current window
         
+        /*
         int result = 0;
         Map<Character, Integer> hashmap = new HashMap<>();
         
@@ -30,6 +31,38 @@ class Solution {
                 
                 // potential new max
                 for (int num : hashmap.values()) {
+                    max = Math.max(max, num);
+                }
+            } 
+            
+            result = Math.max(result, r - l + 1);
+        }
+        
+        return result;
+        */
+        
+        // approach: array of char values 0-26 : frequency in current window
+        
+        int result = 0;
+        int[] counts = new int[26];
+        
+        // window size: r - l + 1
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+            counts[s.charAt(r) - 'A']++;
+            
+            // get max freq in current window
+            int max = 0;
+            for (int num : counts) {
+                max = Math.max(max, num);
+            }
+            
+            while ((r - l + 1) - max > k) {        
+                counts[s.charAt(l) - 'A']--;
+                l++;
+                
+                // potential new max
+                for (int num : counts) {
                     max = Math.max(max, num);
                 }
             } 
