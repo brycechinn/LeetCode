18d9f9ed78e239177d83a8753fa76792f7d15d67
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-06-17 12:59:50 -0700

    Time: 4 ms (17.55%), Space: 43.5 MB (37.78%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    List<String> result = new ArrayList<>();
+    Stack<String> stack = new Stack<>();
+    
+    public List<String> generateParenthesis(int n) {
+        // approach: backtracking using stack.pop()
+        
+        stack.push("(");
+        backtrack(n, 1, 0);
+        
+        return result;
+    }
+    
+    private void backtrack(int n, int numOpen, int numClosed) {
+        if (numOpen >= n && numClosed >= n) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stack.stream().forEach(p -> stringBuilder.append(p));
+            
+            result.add(stringBuilder.toString());
+        }
+        
+        if (numOpen < n) {
+            stack.push("(");
+            backtrack(n, numOpen + 1, numClosed);
+            stack.pop();
+        }
+        
+        if (numClosed < n && numClosed < numOpen) {
+            stack.push(")");
+            backtrack(n, numOpen, numClosed + 1);
+            stack.pop();
+        }
+    }
+}
