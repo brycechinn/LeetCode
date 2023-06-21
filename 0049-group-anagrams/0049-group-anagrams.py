af410657883e76c46b15562a623d88ac55dfc5d3
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-06-21 15:38:54 -0700

    Time: 133 ms (23.01%), Space: 22.4 MB (9.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,6 +1,6 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashmap = {}
+        hashmap = collections.defaultdict(list)
         
         for s in strs:
             counts = [0] * 26
@@ -9,10 +9,6 @@ class Solution:
                 counts[ord(c) - ord('a')] += 1
             
             key = tuple(counts)
-            
-            if key not in hashmap:
-                hashmap[key] = []
-            
             hashmap[key].append(s)
         
         return hashmap.values()
