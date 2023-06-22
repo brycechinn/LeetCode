11d8d0973b2d24cbcf1603ab3a0fa54adc9d2f8d
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-06-22 16:18:20 -0700

    Time: 89 ms (37.74%), Space: 17 MB (9.05%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -1,36 +1,38 @@
 class Codec:
-
-    def encode(self, strs):
+    # approach: encode strings as length + '#' + str
+    
+    def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
-        
-        :type strs: List[str]
-        :rtype: str
         """
         
-        res = ''
+        result = ''
+        
         for s in strs:
-            res += str(len(s)) + '#' + s
-        return res
+            length = str(len(s))
+            result += (length + '#' + s)
 
-    def decode(self, s):
+        return result
+            
+
+    def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
-        
-        :type s: str
-        :rtype: List[str]
         """
+
+        result = []
+        i = 0
         
-        res, i = [], 0
-        
-        while i < (len(s)):
+        while i < len(s):
             j = i
+            
             while s[j] != '#':
                 j += 1
-            num = int(s[i : j])
-            string = s[j + 1 : j + 1 + num]
-            res.append(string)
-            i = j + 1 + num
-        return res
             
+            length = int(s[i : j])
+            result.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+        
+        return result
+
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.decode(codec.encode(strs))
