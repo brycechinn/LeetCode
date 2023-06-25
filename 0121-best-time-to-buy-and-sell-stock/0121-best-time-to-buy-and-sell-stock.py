ca4750b54f03eecc77eec9db9b899bb4eb2f6ed8
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-06-24 19:11:06 -0700

    Time: 1040 ms (46.54%), Space: 27.3 MB (92.37%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,39 +1,11 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        
-        # approach 1: store low and maxProfit
-
-        '''
-        low, maxProfit = prices[0], 0
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        result = 0
+        lowest = prices[0]
         
         for price in prices:
-            low = min(low, price)
-            maxProfit = max(maxProfit, price - low)
+            lowest = min(lowest, price)
+            profit = price - lowest
+            result = max(result, profit)
         
-        return maxProfit
-        '''
-    
-        # approach 2: two pointers
-        
-        l, r = 0, 1
-        maxProfit = 0
-        
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                maxProfit = max(maxProfit, profit)
-            else:
-                l = r
-            
-            r += 1
-        
-        return maxProfit
-                
-                
-    
-        
-        
+        return result
