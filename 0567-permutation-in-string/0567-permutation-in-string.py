62b807127ba7603c2737f12d546144c72e363abe
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-06-25 16:13:03 -0700

    Time: 88 ms (64.44%), Space: 16.4 MB (56.46%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,35 +1,35 @@
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
         
-        # approach: sliding window, two dicts to store char counts,
-        # compare dicts at each iteration
-        
-        if len(s1) > len(s2): return False
-        
-        d1 = dict.fromkeys(string.ascii_lowercase, 0)
-        d2 = dict.fromkeys(string.ascii_lowercase, 0)
-        
-        for char in s1:
-            d1[char] += 1
-        
+        counts1 = [0] * 26
+        counts2 = [0] * 26
+
         l = 0
-        for r in range(len(s2)):
-            rc = s2[r]
-            d2[rc] += 1
-                
-            while d2[rc] > d1[rc]:
-                lc = s2[l]
-                d2[lc] -= 1
-                l += 1
+        r = 0
+        for i in range(len(s1)):
+            c1 = s1[i]
+            c2 = s2[i]
             
-            if d1 == d2:
-                return True
-            
-        return False
-    
+            counts1[ord(c1) - ord('a')] += 1
+            counts2[ord(c2) - ord('a')] += 1
+            r += 1
         
+        if counts1 == counts2:
+            return True
+        
+        r -= 1
+        while r < len(s2) - 1:
+            counts2[ord(s2[l]) - ord('a')] -= 1
+            l += 1
+            r += 1
+            counts2[ord(s2[r]) - ord('a')] += 1
+
+            if counts1 == counts2:
+                return True
+        
+        return False
+            
+            
+            
