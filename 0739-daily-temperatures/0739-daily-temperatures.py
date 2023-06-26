825c21b9f580e60c4d5d70df5fd547ea6e9a1f7f
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-06-26 12:00:25 -0700

    Time: 1289 ms (89.87%), Space: 31.3 MB (35.07%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,40 +1,15 @@
-class Solution(object):
-    def dailyTemperatures(self, temperatures):
-        """
-        :type temperatures: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         
-        # approach 1: brute force, nested loop
-        
-        '''
-        res = []
-        
-        for i, temp in enumerate(temperatures):
-            j = i
-            while j < len(temperatures) and temperatures[j] <= temp:
-                j += 1
-            
-            if j == len(temperatures): # no warmer temperature
-                res.append(0)
-            else:
-                res.append(j - i)
-        
-        return res
-        '''
-        
-        # approach 2: monotonic decreasing stack
-        
-        res, stack = [0] * len(temperatures), []
+        result = [0] * len(temperatures)
+        stack = []
         
         for i, temp in enumerate(temperatures):
             while stack and temp > temperatures[stack[-1]]:
-                top = stack.pop()
-                res[top] = i - top
-
-            stack.append(i)
-        return res
-                
-                    
+                prev = stack.pop()
+                result[prev] = i - prev
             
+            stack.append(i)
         
+        return result
+                
