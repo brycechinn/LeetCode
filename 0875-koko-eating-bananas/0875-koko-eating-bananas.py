5f4f0fb5f6b83f1fe2aab19c3b6958c8adbc800a
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-06-26 13:04:58 -0700

    Time: 483 ms (56.93%), Space: 18 MB (45.74%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,43 +1,23 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # approach: binary search on range [1, max(piles)]
         
-        # approach 1: start with k = min(piles), increment k until
-        # time <= h
-        
-        '''
-        k = 1
-        total = float(inf)
-    
-        while total > h:
-            total = 0
-            for pile in piles:
-                hours = ceil(pile / k)
-                total += hours
-
-            if total > h:
-                k += 1
-        
-        return k
-        '''
-    
-        # approach 2: optimize brute force with binary search on 
-        # range [1, max(piles)]
-        
-        l, r = 1, max(piles)
-        res = r
-        
+        result = float('inf')
+        l = 1
+        r = max(piles)
         while l <= r:
-            k = (l + r) // 2
+            m = (l + r) // 2
             
-            total = 0
+            hours = 0
             for pile in piles:
-                hours = ceil(pile / k)
-                total += hours
+                hours += math.ceil(pile / m)
 
-            if total <= h:
-                res = min(res, k)
-                r = k - 1
+            if hours > h:
+                l = m + 1
             else:
-                l = k + 1
+                r = m - 1
+                
+                # potential new min k
+                result = min(result, m)
         
-        return res
+        return result
