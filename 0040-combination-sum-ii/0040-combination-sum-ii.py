9197ecc740cce3838d662fb8e27410b8a9d999cb
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-04 00:51:57 -0700

    Time: 114 ms (29.98%), Space: 16.6 MB (18.90%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -4,23 +4,19 @@ class Solution:
         # entering right decision branch
         
         candidates.sort()
-        
         combo, res = [], []
         
         def dfs(i, total):
             if total == target:
                 res.append(combo.copy())
-                print(combo)
                 return
             
             if total > target or i == len(candidates):
                 return
-            
-            # include num
+
             combo.append(candidates[i])
             dfs(i + 1, total + candidates[i])
-            
-            # don't include num
+
             combo.pop()
             while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
                 i += 1
