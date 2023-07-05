f20c8e3d9266b27ba34fe32f9eb43edaab5edb76
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-05 15:24:29 -0700

    Time: 67 ms (75.03%), Space: 17 MB (18.46%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        res = []
+        cols = set()
+        pos_diags = set() # r + c
+        neg_diags = set() # r - c
+        
+        def dfs(r):
+            if r == n:
+                copy = [''.join(row) for row in board]
+                res.append(copy)
+                return
+            
+            for c in range(n):
+                if (c in cols or 
+                    r + c in pos_diags or 
+                    r - c in neg_diags):
+                    continue
+            
+                # place queen
+                board[r][c] = 'Q'
+                cols.add((c))
+                pos_diags.add((r + c))
+                neg_diags.add((r - c))
+                dfs(r + 1)
+                
+                # backtrack
+                board[r][c] = '.'
+                cols.remove((c))
+                pos_diags.remove((r + c))
+                neg_diags.remove((r - c))
+        
+        dfs(0)
+        return res
