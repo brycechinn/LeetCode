a159138d13dc6d0ff65f21157826b639b2b5d961
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-10 16:19:14 -0700

    Time: 136 ms (91.79%), Space: 19.7 MB (54.70%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -2,70 +2,39 @@ class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         if endWord not in wordList:
             return 0
-        
-        # adj = collections.defaultdict(set)
+
         visited = set()
-        dists = {}
+        adj = collections.defaultdict(list)
+        res = 1
         
         wordList.append(beginWord)
-        
-        '''
-        for w1 in wordList:
-            for w2 in wordList:
-                if w1 != w2 and self.areAdjacent(w1, w2):
-                    adj[w1].add(w2)
-                    adj[w2].add(w1)
-        '''
-        
-        adj = collections.defaultdict(list)
-        
+       
         for w in wordList:
             for i in range(len(w)):
                 pattern = w[:i] + '*' + w[i + 1:]
                 adj[pattern].append(w)
-        
-        for w in wordList:
-            dists[w] = float('inf')
-        
-        dists[beginWord] = 0
 
-        def bfs():
-            q = collections.deque()
-            q.append(beginWord)
-            
-            while q:
+        q = collections.deque()
+        q.append(beginWord)
+
+        while q:
+            size = len(q)
+            for i in range(size):
                 w = q.popleft()
-                
+
+                if w == endWord:
+                    return res
+
                 for i in range(len(w)):
                     pattern = w[:i] + '*' + w[i + 1:]
-                    
+
                     for n in adj[pattern]:
                         if n in visited:
                             continue
-                    
+
                         visited.add(n)
-                        dists[n] = min(dists[n], dists[w] + 1)
                         q.append(n)
-                
-                for n in adj[w]:
-                    if n in visited:
-                        continue
-                    
-                    visited.add(n)
-                    dists[n] = min(dists[n], dists[w] + 1)
-                    q.append(n)
 
-        bfs()
-        res = dists[endWord]
+            res += 1
 
-        return res + 1 if res != float('inf') else 0
-            
-        
-    def areAdjacent(self, w1, w2):
-        differences = 0
-        
-        for i in range(len(w1)):
-            if w1[i] != w2[i]:
-                differences += 1
-        
-        return differences == 1
+        return 0
