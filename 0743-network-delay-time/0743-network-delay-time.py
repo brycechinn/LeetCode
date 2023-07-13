99a9af555a2a12af867c544ce4a936cf89b93c17
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-13 16:30:09 -0700

    Time: 488 ms (38.52%), Space: 18.8 MB (23.93%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,34 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # approach: Djikstra's Shortest Path from node k
+        
+        # 1. build adjacency list
+        adj = collections.defaultdict(list)
+        
+        for u, v, w in times:
+            adj[u].append((w, v))
+        
+        # 2. Djikstra's via BFS with min heap of (dist from source, node)
+        dists = [float('inf')] * (n + 1)
+        visited = set()
+
+        def bfs(src):
+            heap = []
+            heap.append((0, src))
+
+            while heap:
+                path, i = heapq.heappop(heap)
+                
+                if i in visited:
+                    continue
+                
+                visited.add(i)
+                dists[i] = min(dists[i], path)
+                
+                for dist, nei in adj[i]:
+                    if nei not in visited:
+                        heapq.heappush(heap, (path + dist, nei))
+                
+        bfs(k)
+        # print(dists)
+        return max(dists[1:]) if len(visited) == n else -1
