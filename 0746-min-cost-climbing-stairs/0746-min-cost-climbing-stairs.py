5164aa0aed13e6ada7e65dca0ba7f894f223b9ed
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-15 16:40:10 -0700

    Time: 72 ms (66.60%), Space: 16.5 MB (34.38%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -0,0 +1,41 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # approach 1: DFS (TLE)
+        '''
+        cost.insert(0, 0)
+        n = len(cost)
+        res = float('inf')
+
+        def dfs(i, total):
+            nonlocal res
+            
+            if i > n:
+                return
+            
+            if i == n:
+                res = min(res, total)
+                return
+            
+            total += cost[i]
+            
+            dfs(i + 1, total)
+            dfs(i + 2, total)
+        
+        dfs(0, 0)
+        return res
+        '''
+        
+        # approach 2: bottom-up DP
+        cost.insert(0, 0)
+        n = len(cost)
+        one, two = cost[-1], 0
+        
+        for i in range(n - 2, -1, -1):
+            new = min(cost[i] + one, cost[i] + two)
+            # print('cost[i]: {} one: {} two: {}'.format(cost[i], one, two))
+            two = one
+            one = new
+        
+        return one
+            
+            
