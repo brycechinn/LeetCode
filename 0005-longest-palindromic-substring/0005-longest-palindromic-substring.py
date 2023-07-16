dd5de82021f8d3ec089d7e26f6b313187463f2d5
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-16 15:20:40 -0700

    Time: 693 ms (66.00%), Space: 16.3 MB (95.78%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,29 +3,21 @@ class Solution:
         # approach: iterate through s while expanding around center char, while loops
         # for odd and even length
         
-        start, end = 0, 0
-        longest = 0
-        palindromes = set()
+        longest, start, end = 0, 0, 0
+
+        def expand(l, r):
+            nonlocal longest, start, end
+            
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > longest:
+                    start, end = l, r
+                    longest = r - l + 1
+                
+                l -= 1
+                r += 1
         
         for i in range(len(s)):
-            # odd length
-            l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > longest:
-                    start, end = l, r
-                    longest = r - l + 1
-                
-                l -= 1
-                r += 1
-                
-            # even length
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > longest:
-                    start, end = l, r
-                    longest = r - l + 1
-                
-                l -= 1
-                r += 1
-            
+            expand(i, i)     # odd length
+            expand(i, i + 1) # even length
+        
         return s[start:end + 1]
