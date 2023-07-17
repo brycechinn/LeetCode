8bd9fc0a5ee7e05ded438b187b1b09386c3a0641
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-17 14:57:16 -0700

    Time: 1242 ms (79.67%), Space: 16.6 MB (95.08%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,20 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # approach: bottom-up DP, tabulation: i : minimum num of coins that sum to i
+        
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        
+        for c in coins:
+            if c <= amount:
+                dp[c] = 1
+        
+        for i in range(1, amount + 1):
+            for c in coins:
+                if c > i:
+                    continue
+                    
+                # recurrence relation
+                dp[i] = min(dp[i], 1 + dp[i - c])
+
+        return dp[amount] if dp[amount] != float('inf') else -1
