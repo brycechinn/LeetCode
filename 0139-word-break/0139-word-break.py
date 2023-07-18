de07e2112776cafd3cf6df35d270169259ac8fe1
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-17 18:08:08 -0700

    Time: 60 ms (31.15%), Space: 16.6 MB (31.97%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # approach: bottom-up DP via iteration, tabulation of 
+        # i : can remaining string be broken?
+
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
+                if i + len(w) <= len(s) and s[i:i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                    
+                if dp[i]:
+                    break
+
+        return dp[0]
