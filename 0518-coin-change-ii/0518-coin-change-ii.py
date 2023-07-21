4f6f0a87614cec0bf5875cc306c402c3ae0987e0
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-21 01:09:42 -0700

    Time: 581 ms (33.13%), Space: 26.2 MB (55.60%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -3,6 +3,7 @@ class Solution:
         # approach 1: top-down DP via memoization with cache of 
         # (i, total) : unique combinations
 
+        '''
         dp = {}
         
         def dfs(i, remaining):
@@ -19,3 +20,25 @@ class Solution:
             return dp[(i, remaining)]
         
         return dfs(0, amount)
+        '''
+        
+        # approach 2: bottom-up DP via tabulation
+        
+        m, n = len(coins), amount + 1
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        
+        for r in range(m):
+            dp[r][n - 1] = 1
+        
+        for r in range(m - 1, -1, -1):
+            for c in range(n - 2, -1, -1):
+                dp[r][c] = dp[r + 1][c] if r + 1 < m else 0
+                
+                if c + coins[r] < n:
+                    dp[r][c] += dp[r][c + coins[r]]
+
+        return dp[0][0]
+        
+    def display(self, grid):
+        for r in range(len(grid)):
+            print(grid[r])
