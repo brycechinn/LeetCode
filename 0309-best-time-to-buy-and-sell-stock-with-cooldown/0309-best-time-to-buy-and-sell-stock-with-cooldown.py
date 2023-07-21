02ccf0a08f3923ab44ea2955144241f86a06bba6
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-20 18:14:11 -0700

    Time: 64 ms (41.07%), Space: 21.5 MB (25.23%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # approach: top-down memoization via DFS and cache of
+        # (i, holding) : max_profit
+        
+        dp = {}
+
+        def dfs(i, holding):
+            if i >= len(prices):
+                return 0
+            
+            if (i, holding) in dp:
+                return dp[(i, holding)]
+            
+            # cooldown
+            c = dfs(i + 1, holding)
+            if holding:
+                # sell
+                s = dfs(i + 2, False) + prices[i]
+                dp[(i, holding)] = max(c, s)
+            else:
+                # buy
+                b = dfs(i + 1, True) - prices[i]
+                dp[(i, holding)] = max(c, b)
+            
+            return dp[(i, holding)]
+                
+        return dfs(0, False)
