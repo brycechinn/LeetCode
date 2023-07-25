daa0501da13196b0f8a31e1a27f595e7cc31d2aa
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-25 13:56:35 -0700

    Time: 58 ms (65.10%), Space: 17.2 MB (43.10%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,31 +1,26 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        # approach 1: top-down DP, memoization of (i1, i2) : isInterleaving
+
         if len(s1) + len(s2) != len(s3):
             return False
         
         dp = {}
         
-        def dfs(i1, i2):
-            if (i1, i2) in dp:
-                return dp[(i1, i2)]
+        def dfs(i, j):
+            if (i, j) in dp:
+                return dp[(i, j)]
             
-            if i1 == len(s1) and i2 == len(s2):
+            if i == len(s1) and j == len(s2):
                 return True
             
-            c1 = s1[i1] if i1 < len(s1) else ''
-            c2 = s2[i2] if i2 < len(s2) else ''
-            c3 = s3[i1 + i2]
+            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
+                return True
             
-            if c1 != c3 and c2 != c3:
-                return False
+            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+                return True
             
-            if c1 == c3 and c2 == c3:
-                dp[(i1, i2)] = dfs(i1 + 1, i2) or dfs(i1, i2 + 1)
-            elif c1 == c3:
-                dp[(i1, i2)] = dfs(i1 + 1, i2)
-            else:
-                dp[(i1, i2)] = dfs(i1, i2 + 1)
-            
-            return dp[(i1, i2)]
+            dp[(i, j)] = False
+            return False
         
         return dfs(0, 0)
