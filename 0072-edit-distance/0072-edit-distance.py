57e160ba4f58b891a9218f2bb3b6f4cee6f88c82
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-26 12:01:40 -0700

    Time: 180 ms (30.36%), Space: 26.1 MB (7.24%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,7 +1,7 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         # approach 1: top-down DP, memoization of (i, j) : min operations
-        
+        '''
         dp = {}
         
         def dfs(i, j):
@@ -22,4 +22,32 @@ class Solution:
                                      dfs(i + 1, j + 1)) # replace
             return dp[(i, j)]
 
-        return dfs(0, 0)           
+        return dfs(0, 0)
+        '''
+        # approach 2: bottom-up DP, tabulation where dp[i][j] = min operations
+        # to convert word1[i:] to word2[j:]
+        
+        m, n = len(word1) + 1, len(word2) + 1
+        dp = [[float('inf') for _ in range(n)] for _ in range(m)]
+        
+        for r in range(m):
+            dp[r][n - 1] = m - 1 - r
+        
+        for c in range(n):
+            dp[m - 1][c] = n - 1 - c
+        
+        for r in range(m - 2, -1, -1):
+            for c in range(n - 2, -1, -1):
+                if word1[r] == word2[c]:
+                    dp[r][c] = dp[r + 1][c + 1]
+                else:
+                    dp[r][c] = 1 + min(dp[r][c + 1],     # insert
+                                       dp[r + 1][c],     # delete
+                                       dp[r + 1][c + 1]) # replace
+
+        return dp[0][0]
+        
+    def display(self, grid):
+        for r in range(len(grid)):
+            print(grid[r])
+        
