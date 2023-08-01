bcd80632481d5d6097cf3ec915a98d515359a9aa
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-31 17:37:06 -0700

    Time: 568 ms (64.14%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -3,12 +3,12 @@
 -- just count managerId and if it's >= 5, output the manager's name
 
 SELECT name
-FROM Employee e
+FROM Employee AS e
 JOIN (
-    SELECT managerId, COUNT(managerId) AS direct_reports
+    SELECT managerId
     FROM Employee
     WHERE managerId IS NOT NULL
     GROUP BY managerId
-) m
+    HAVING COUNT(managerId) >= 5
+) AS m
 ON e.id = m.managerId
-WHERE direct_reports >= 5;
