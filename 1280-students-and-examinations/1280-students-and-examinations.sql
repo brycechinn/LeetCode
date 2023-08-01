d608e53d1691a6b3294c3080f54374dff175dba6
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-07-31 17:09:16 -0700

    Time: 1848 ms (76.35%), Space: 0B (100.00%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,4 +1,10 @@
 # Write your MySQL query statement below
+-- 1. create subquery "grouped" to count number of exams each student 
+--    attended for each subject
+-- 2. cross join each row from Student and Subject to get all 
+--    combinations of (student_id, subject_name)
+-- 3. left join "grouped" with this table
+
 SELECT stu.student_id, stu.student_name, sub.subject_name, IFNULL(grouped.attended_exams, 0) AS attended_exams
 FROM Students stu
 CROSS JOIN Subjects  sub
