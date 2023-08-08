1e8a8983c1f603c37856251bf66d1bfd62a56e99
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-08-07 17:47:43 -0700

    Time: 82 ms (85.33%), Space: 19.9 MB (29.94%) - LeetHub

---
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        # approach: sort intervals then compare in pairs
+        
+        intervals.sort(key=lambda x: x[0])
+        
+        for i in range(len(intervals) - 1):
+            if intervals[i][1] > intervals[i + 1][0]:
+                return False
+        
+        return True
+        
+        
