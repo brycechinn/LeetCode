0b62f33048e4ad28a0ee2025a8dad5c8d2934d23
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-08-09 16:23:03 -0700

    Time: 117 ms (90.28%), Space: 17.4 MB (20.15%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,6 +4,10 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         
+        # approach 1: store rows and cols to zero in sets
+        # time: O(mn)
+        # space: O(m + n)
+        '''
         m, n = len(matrix), len(matrix[0])
         rows, cols = set(), set()
         
@@ -20,3 +24,35 @@ class Solution:
         for c in cols:
             for r in range(m):
                 matrix[r][c] = 0    
+        '''
+        
+        # approach 2: store rows and cols to zero in matrix itself,
+        # use a single variable to represent row zero
+        # time: O(mn)
+        # space: O(1)
+        
+        m, n = len(matrix), len(matrix[0])
+        row_zero = False
+        
+        for r in range(m):
+            for c in range(n):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        row_zero = True
+        
+        for r in range(1, m):
+            for c in range(1, n):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+        
+        if matrix[0][0] == 0:
+            for r in range(m):
+                matrix[r][0] = 0
+        
+        if row_zero:
+            for c in range(n):
+                matrix[0][c] = 0
