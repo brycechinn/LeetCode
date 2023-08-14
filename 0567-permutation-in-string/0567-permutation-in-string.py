6c281c13b837901475a7820e18bf4bc532c2afc8
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-08-14 16:54:42 -0700

    Time: 173 ms (28.97%), Space: 16.5 MB (58.51%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -5,25 +5,26 @@ class Solution:
         if len(s1) > len(s2):
             return False
         
-        d1, d2 = {}, {}
+        d1 = collections.defaultdict(int)
+        d2 = collections.defaultdict(int)
         
         for c in s1:
-            d1[c] = 1 + d1.get(c, 0)
+            d1[c] += 1
 
         l = 0
         for r in range(len(s2)):
             head = s2[r]
-            d2[head] = 1 + d2.get(head, 0)
+            d2[head] += 1
             
             if head not in d1 or d1[head] < d2[head]:
-                while d1.get(head, 0) != d2.get(head, 0) and l <= r:
+                while d1[head] != d2[head] and l <= r:
                     tail = s2[l]
                     d2[tail] -= 1
                     l += 1
             
             matches = 0
             for c in d1:
-                if d1.get(c, 0) == d2.get(c, 0):
+                if d1[c] == d2[c]:
                     matches += 1
             
             if matches == len(d1):
