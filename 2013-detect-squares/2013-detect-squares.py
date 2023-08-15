431fdbac60dd173e9d9157e170934abf38fc49cb
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-08-15 16:27:52 -0700

    Time: 557 ms (31.10%), Space: 18.7 MB (27.34%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -1,26 +1,26 @@
 class DetectSquares:
-    # approach: hashmap of point : count, list of points, look for diagonal points, 
-    # then check if corners exist in hashmap
+    # approach: list of points, hashmap of points
 
     def __init__(self):
-        self.counts, self.points = {}, []
+        self.points = []
+        self.counts = {}
 
     def add(self, point: List[int]) -> None:
-        self.counts[tuple(point)] = 1 + self.counts.get(tuple(point), 0)
         self.points.append(point)
+        self.counts[tuple(point)] = 1 + self.counts.get(tuple(point), 0)
 
     def count(self, point: List[int]) -> int:
         x1, y1 = point
-        res = 0
+        total = 0
         
         for x2, y2 in self.points:
-            if abs(x2 - x1) != abs(y2 - y1) or x1 == x2 or y1 == y2:
+            if abs(x1 - x2) != abs(y1 - y2) or x1 == x2 or y1 == y2:
                 continue
                 
             if (x1, y2) in self.counts and (x2, y1) in self.counts:
-                res += self.counts[(x1, y2)] * self.counts[(x2, y1)]
+                total += self.counts[(x1, y2)] * self.counts[(x2, y1)]
         
-        return res
+        return total
 
 # Your DetectSquares object will be instantiated and called as such:
 # obj = DetectSquares()
