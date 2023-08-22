189d2efbf0460b8731f8fc8c461b3502b8ae4721
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-08-22 14:30:01 -0700

    Time: 125 ms (63.67%), Space: 17.4 MB (18.36%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,10 +4,8 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         
-        # approach: arrays two arrays to store rows/cols to be zeroed,
-        # optimize by embedding arrays in matrix with extra left corner
-        # variable
-        
+        # approach 1: two arrays to store rows/cols to be zeroed
+        '''
         m, n = len(matrix), len(matrix[0])
         
         rows, cols = [False] * m, [False] * n
@@ -27,3 +25,35 @@ class Solution:
             if cols[c]:
                 for r in range(m):
                     matrix[r][c] = 0
+        '''
+        # approach 2: optimize by embedding arrays in matrix with 
+        # variable for overlapping top left corner
+        
+        m, n = len(matrix), len(matrix[0])
+        
+        row_zero = False
+        
+        for r in range(m):
+            for c in range(n):
+                if matrix[r][c] == 0:
+                    if r == 0:
+                        row_zero = True                   
+                    else:
+                        matrix[r][0] = 0
+                        
+                    matrix[0][c] = 0
+        
+        for r in range(1, m):
+            for c in range(1, n):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
+                    matrix[r][c] = 0
+        
+        if matrix[0][0] == 0:
+            for r in range(m):
+                matrix[r][0] = 0
+        
+        if row_zero:
+            for c in range(n):
+                matrix[0][c] = 0
+            
+        print(matrix)
