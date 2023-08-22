711c132b4609eeb455d00d871419336b7daedc71
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-08-22 15:28:14 -0700

    Time: 61 ms (18.42%), Space: 17 MB (20.90%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,11 +1,10 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # approach: binary search to find row, then binary search
-        # to find num within row
+        # approach: two binary searches, one to find row and one to
+        # find num within row
         
-        l = 0
-        r = len(matrix) - 1
-        m = 0
+        l, r, m = 0, len(matrix) - 1, 0
+        
         while l <= r:
             m = (l + r) // 2
             row = matrix[m]
@@ -18,14 +17,15 @@ class Solution:
                 break
         
         row = matrix[m]
-        l = 0
-        r = len(row) - 1
+        l, r = 0, len(row) - 1
+        
         while l <= r:
             m = (l + r) // 2
+            num = row[m]
             
-            if row[m] < target:
+            if target > num:
                 l = m + 1
-            elif row[m] > target:
+            elif target < num:
                 r = m - 1
             else:
                 return True
