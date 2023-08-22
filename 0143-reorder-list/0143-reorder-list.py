31ca91028453b38d8f23239858a108baf07c04e8
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-08-22 13:01:45 -0700

    Time: 97 ms (48.79%), Space: 26.3 MB (27.28%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -9,34 +9,30 @@ class Solution:
         Do not return anything, modify head in-place instead.
         """
         
-        # approach:
-        # 1. find middle of list with slow and fast ptr
-        # 2. reverse right half of list
-        # 3. alternate adding nodes from left and right half, working inward
+        # approach: split list in half, reverse right half, alternate
+        # nodes
         
-        # 1. find middle of list with slow and fast ptr
-        slow = head
-        fast = head
+        # get midpoint
+        slow, fast = head, head
         
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
-        # 2. reverse right half of list
-        prev = None
-        curr = slow.next
-        slow.next = None
-        
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
 
+        right = slow.next
+        slow.next = None
         left = head
-        right = prev
         
-        # 3. alternate adding nodes from left and right half, working inward
+        # reverse right half
+        prev = None
+        while right:
+            temp = right.next
+            right.next = prev
+            prev = right
+            right = temp
+
+        # reorder list
+        right = prev
         while left and right:
             temp = left.next
             left.next = right
@@ -45,5 +41,3 @@ class Solution:
             temp = right.next
             right.next = left
             right = temp
-        
-        return head
