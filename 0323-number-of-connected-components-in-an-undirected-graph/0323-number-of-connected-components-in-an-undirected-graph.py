26c00df9658f51cc2630ce0d364d16ccc2f291bf
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-08-22 16:15:09 -0700

    Time: 94 ms (94.20%), Space: 17.7 MB (74.00%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,7 +1,7 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         # approach 1: DFS
-        
+        '''
         adj = {}
         
         for node, nei in edges:
@@ -36,4 +36,42 @@ class Solution:
             dfs(i)
             res += 1
                 
+        return res
+        '''
+    
+        # approach 2: union find
+        
+        par = [i for i in range(n)]
+        rank = [1] * n
+        
+        def find(n):
+            p = par[n]
+            
+            while p != par[p]:
+                p = par[p]
+            
+            return p
+            
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            
+            if p1 == p2:
+                return False
+            
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            
+            return True
+        
+        res = n
+        for n1, n2 in edges:
+            if not union(n1, n2):
+                continue
+            
+            res -= 1
+        
         return res
