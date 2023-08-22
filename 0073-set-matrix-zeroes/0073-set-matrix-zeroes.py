bd18f3a1569b509b3485805d0466d0038bd4dde1
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-08-22 14:00:51 -0700

    Time: 119 ms (75.51%), Space: 17.3 MB (55.91%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,55 +4,26 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         
-        # approach 1: store rows and cols to zero in sets
-        # time: O(mn)
-        # space: O(m + n)
-        '''
+        # approach: arrays two arrays to store rows/cols to be zeroed,
+        # optimize by embedding arrays in matrix with extra left corner
+        # variable
+        
         m, n = len(matrix), len(matrix[0])
-        rows, cols = set(), set()
+        
+        rows, cols = [False] * m, [False] * n
         
         for r in range(m):
             for c in range(n):
                 if matrix[r][c] == 0:
-                    rows.add(r)
-                    cols.add(c)
-        
-        for r in rows:
-            for c in range(n):
-                matrix[r][c] = 0
-        
-        for c in cols:
-            for r in range(m):
-                matrix[r][c] = 0    
-        '''
-        
-        # approach 2: store rows and cols to zero in matrix itself,
-        # use a single variable to represent row zero
-        # time: O(mn)
-        # space: O(1)
-        
-        m, n = len(matrix), len(matrix[0])
-        row_zero = False
-        
+                    rows[r] = True
+                    cols[c] = True
+
         for r in range(m):
-            for c in range(n):
-                if matrix[r][c] == 0:
-                    matrix[0][c] = 0
-                
-                    if r > 0:
-                        matrix[r][0] = 0
-                    else:
-                        row_zero = True
-        
-        for r in range(1, m):
-            for c in range(1, n):
-                if matrix[0][c] == 0 or matrix[r][0] == 0:
+            if rows[r]:
+                for c in range(n):
                     matrix[r][c] = 0
         
-        if matrix[0][0] == 0:
-            for r in range(m):
-                matrix[r][0] = 0
-        
-        if row_zero:
-            for c in range(n):
-                matrix[0][c] = 0
+        for c in range(n):
+            if cols[c]:
+                for r in range(m):
+                    matrix[r][c] = 0
