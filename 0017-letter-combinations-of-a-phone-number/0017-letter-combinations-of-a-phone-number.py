a968b243dc4ec735f1ccff435729739aace853b4
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-08-22 14:55:06 -0700

    Time: 33 ms (90.79%), Space: 16.4 MB (51.87%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,37 +1,34 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        # approach: hashmap of digit : list of chars, backtracking
+        
         if not digits:
             return []
         
-        hashmap = { 
-            '2': ['a', 'b', 'c'],
-            '3': ['d', 'e', 'f'],
-            '4': ['g', 'h', 'i'],
-            '5': ['j', 'k', 'l'],
-            '6': ['m', 'n', 'o'],
-            '7': ['p', 'q', 'r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', 'z']
+        letters = {
+            '2' : 'abc',
+            '3' : 'def',
+            '4' : 'ghi',
+            '5' : 'jkl',
+            '6' : 'mno',
+            '7' : 'pqrs',
+            '8' : 'tuv',
+            '9' : 'wxyz'
         }
         
-        curr, res = [], []
+        res, combo = [], []
         
         def dfs(i):
             if i == len(digits):
-                res.append(''.join(curr))
+                res.append(''.join(combo))
                 return
             
             d = digits[i]
-            for c in hashmap[d]:
-                # include char
-                curr.append(c)
-                dfs(i + 1)
-                
-                # backtrack
-                curr.pop()
             
+            for c in letters[d]:
+                combo.append(c)
+                dfs(i + 1)
+                combo.pop()
+                
         dfs(0)
         return res
-                
-            
-            
