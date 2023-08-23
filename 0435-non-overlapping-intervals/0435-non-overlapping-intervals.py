1154391d3cb411804d759f719048d6a893e4a1f8
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-08-23 15:14:17 -0700

    Time: 1280 ms (75.60%), Space: 55.2 MB (79.71%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -3,15 +3,15 @@ class Solution:
         # approach: sort intervals, then compare current start to
         # previous end
         
-        intervals.sort(key=lambda x: x[0])
-        res = 0
+        intervals.sort()
         prev_end = intervals[0][1]
+        res = 0
         
-        for start, end in intervals[1:]:
-            if start >= prev_end:
-                prev_end = end
+        for curr_start, curr_end in intervals[1:]:
+            if curr_start >= prev_end:
+                prev_end = curr_end
             else:
                 res += 1
-                prev_end = min(prev_end, end)
+                prev_end = min(prev_end, curr_end)
         
         return res
