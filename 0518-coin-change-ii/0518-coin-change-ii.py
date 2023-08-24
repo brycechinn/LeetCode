e8b0315afe00cd9ae7c9329431537fabbb82095f
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-08-23 19:12:04 -0700

    Time: 975 ms (11.70%), Space: 27.4 MB (46.52%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -24,7 +24,7 @@ class Solution:
         return res
         '''
         # approach 2: memoization
-        
+        '''
         dp = {}
         
         def dfs(i, remaining):
@@ -43,3 +43,23 @@ class Solution:
             return dp[(i, remaining)]
             
         return dfs(0, amount)
+        '''
+        # approach 3: bottom-up DP
+        
+        m, n = amount + 1, len(coins)
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        
+        for c in range(n):
+            dp[m - 1][c] = 1 
+        
+        for r in range(m - 2, -1, -1):
+            for c in range(n - 1, -1, -1):
+                down = dp[r + coins[c]][c] if r + coins[c] in range(m) else 0
+                right = dp[r][c + 1] if c + 1 in range(n) else 0
+                dp[r][c] = down + right
+   
+        return dp[0][0]
+        
+    def display(self, board):
+        for row in board:
+            print(row)
