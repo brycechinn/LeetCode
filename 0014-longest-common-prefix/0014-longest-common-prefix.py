d55384a342cb32ec77e36784a07a7bf8d9c3f219
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2023-12-16 14:42:06 -0800

    Time: 42 ms (54.94%), Space: 16.4 MB (18.66%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,20 +3,10 @@ class Solution:
 
         i = 0
         while True:
-            if i >= len(strs[0]):
-                break
-            
-            c = strs[0][i]
-            match = True
-            
             for s in strs:
-                if i >= len(s) or s[i] != c:
-                    match = False
-                    break
-            
-            if not match:
-                break
+                if i >= len(s) or s[i] != strs[0][i]:
+                    return strs[0][:i]
             
             i += 1
             
-        return strs[0][:i]
+        return ''
