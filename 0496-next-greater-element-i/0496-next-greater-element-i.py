dbcb0123c02ab7cb76668f40bc4f2c1a9fb2f190
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2024-10-22 22:27:07 -0700

    Time: 0 ms (100.00%), Space: 16.8 MB (79.42%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,5 +1,6 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        '''
         # approach 1: O(mn) solution where m = len(nums1), n = len(nums2)
         
         res = []
@@ -18,5 +19,29 @@ class Solution:
             
             if not found:
                 res.append(-1)
+        
+        return res
+        '''
+    
+        # approach 2: O(m + n) solution where m = len(nums1), n = len(nums2)
+        
+        stack, d, res = [], {}, [-1] * len(nums1)
+        
+        for i, num1 in enumerate(nums1):
+            d[num1] = i
+        
+        for num2 in nums2:
+            if not stack:
+                stack.append(num2)
+                continue
+                
+            while stack and num2 > stack[-1]:
+                val = stack.pop()
+                
+                if val in d:
+                    index = d[val]
+                    res[index] = num2
+                
+            stack.append(num2)
         
         return res
