c1df959bba3389c9f7075638473d005b55f3f631
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2024-10-23 23:06:46 -0700

    Time: 0 ms (100.00%), Space: 17.5 MB (97.93%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,5 +1,9 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
+        # approach: use sum of nums to calculate left and right sum
+        # time: O(n)
+        # space: O(1)
+        
         left_sum, right_sum = 0, sum(nums)
         
         for i, num in enumerate(nums):
