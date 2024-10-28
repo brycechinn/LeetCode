6f479f1ff58ef5700b90f78aa1696a70c64c77ed
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2024-10-27 20:26:01 -0700

    Time: 47 ms (12.91%), Space: 24.1 MB (98.49%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -1,6 +1,7 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        # approach 1: hashset to track seen nums
+        '''
+        # approach 1: hashset to track missing nums
         # time: O(n) space: O(n)
         
         missing = set(range(1, len(nums) + 1))
@@ -8,5 +9,14 @@ class Solution:
         for num in nums:
             if num in missing:
                 missing.remove(num)
-        
+                
         return list(missing)
+        '''
+        
+        # approach 2: map indices to values and mark as negative
+        # time: O(n) space: O(1)
+        
+        for i, num in enumerate(nums):
+            nums[abs(num) - 1] = -1 * abs(nums[abs(num) - 1])
+        
+        return [ i + 1 for i, num in enumerate(nums) if num > 0 ]
