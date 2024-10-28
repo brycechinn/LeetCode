a92503dbe07da50eec77d5824b6b9d97d2000054
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2024-10-27 20:49:12 -0700

    Time: 4 ms (23.04%), Space: 16.7 MB (47.50%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        # approach: hashmap of char: count, then compare with target hashmap
+        
+        target, target_map, text_map = 'balloon', defaultdict(int), defaultdict(int)
+        
+        for char in target:
+            target_map[char] += 1
+        
+        for char in text:
+            text_map[char] += 1
+        
+        res = len(text)
+        for char, target_count in target_map.items():
+            text_count = text_map[char]         
+            res = min(res, text_count // target_count)
+        
+        return res
