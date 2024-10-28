6acf26af98c8991082ab2339287c052f168d35f6
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2024-10-27 20:56:07 -0700

    Time: 0 ms (100.00%), Space: 16.7 MB (47.50%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -3,15 +3,9 @@ class Solution:
         # approach: hashmap of char: count, then compare with target hashmap
         # time: O(n) space: O(n)
         
-        target, target_map, text_map = 'balloon', defaultdict(int), defaultdict(int)
-        
-        for char in target:
-            target_map[char] += 1
-        
-        for char in text:
-            text_map[char] += 1
-        
-        res = len(text)
+        target, res = 'balloon', len(text)
+        target_map, text_map= Counter(target), Counter(text)
+
         for char, target_count in target_map.items():
             text_count = text_map[char]         
             res = min(res, text_count // target_count)
