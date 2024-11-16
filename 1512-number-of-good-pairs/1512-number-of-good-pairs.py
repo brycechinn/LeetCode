e8c473e4174d03896600b03a1679fca69f07a685
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2024-11-16 14:32:20 -0800

    Time: 0 ms (100.00%), Space: 16.6 MB (58.07%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -2,7 +2,7 @@ class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         # approach 1: brute force
         # time: O(n^2), space: O(1)
-        
+        '''
         res = 0
         for i in range(len(nums)):
             for j in range(i, len(nums)):
@@ -10,3 +10,17 @@ class Solution:
                     res += 1
         
         return res
+        '''
+    
+        # approach 2: hashmap to count num occurrences, triangular number formula
+        # time: O(n), space: O(n)
+        counts, res = Counter(nums), 0
+        
+        for count in counts.values():
+            res += self.triangularNumberFormula(count - 1)
+        
+        return res
+    
+    def triangularNumberFormula(self, n: int) -> int:
+        # will always be a whole number
+        return (n * (n + 1)) // 2
