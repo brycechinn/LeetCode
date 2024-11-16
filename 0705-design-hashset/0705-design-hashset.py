5d1551ed0043210c4db2fa487cc3bcd26b1cf7ee
Bryce Chinn <33844159+brycechinn@users.noreply.github.com>
2024-11-16 13:27:25 -0800

    Time: 65 ms (39.61%), Space: 22.6 MB (29.77%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -3,7 +3,7 @@ class ListNode:
     A single node of a linked list.
     '''
     
-    def __init__(self, val: int = 0) -> None:
+    def __init__(self, val: int = 0):
         self.val = val
         self.next = None
 
@@ -12,7 +12,7 @@ class LinkedList:
     A linked list with a dummy node. Each node value is unique.
     '''
     
-    def __init__(self) -> None:
+    def __init__(self):
         self.head = ListNode()
         
     def add(self, val: int) -> None:
@@ -67,14 +67,98 @@ class LinkedList:
             curr = curr.next
         
         return False
+
+class TreeNode:
+    def __init__(self, val: int = 0):
+        self.val = val
+        self.left = None
+        self.right = None
+    
+class BST:
+    def __init__(self):
+        self.root = None
+        
+    def add(self, val: int) -> None:
+        '''
+        Inserts a node with the given value into the BST. If it is already present, the
+        BST is not modified.
+        '''
+        
+        def helper(root, val):
+            if not root:
+                return TreeNode(val)
+
+            if val < root.val:
+                root.left = helper(root.left, val)
+            elif val > root.val:
+                root.right = helper(root.right, val)
+            else:
+                # node already in tree, so just return root
+                pass
+            
+            return root
+        
+        self.root = helper(self.root, val)
+        
+    def remove(self, val: int) -> None:
+        '''
+        Removes a node with the given value from the BST. If it is not present,
+        the BST is not modified.
+        '''
+        
+        def findMin(root):
+            if not root.left:
+                return root.val
+            
+            return findMin(root.left)
+        
+        def helper(root, val):
+            if not root:
+                return None
+            
+            if val == root.val:
+                if not root.left:
+                    return root.right
+                
+                if not root.right:
+                    return root.left
+                
+                # set root.val to min of right subtree
+                root.val = findMin(root.right)
+                
+                # delete min of right subtree from right subtree
+                root.right = helper(root.right, root.val)
+            
+            if val < root.val:
+                root.left = helper(root.left, val)
+            else:
+                root.right = helper(root.right, val)
+                
+            return root
+        
+        self.root = helper(self.root, val)
+    
+    def contains(self, val) -> bool:
+        '''
+        Returns True if a node with the specified value exists in the
+        BST, and False otherwise.
+        '''
+        
+        def helper(root, val):
+            if not root or val == root.val:
+                return root
+            
+            return helper(root.left, val) if val < root.val else helper(root.right, val)
+        
+        return helper(self.root, val) is not None
     
 class MyHashSet:
-    # approach: array of buckets, each implemented as linked list
+    # approach: array of buckets, each implemented as BST
     # time: O(1) amortized, space: O(n)
     
     def __init__(self):
         self.size = 997 # largest prime in [1, 1000]
-        self.buckets = [LinkedList() for i in range(self.size)]
+        self.buckets = [BST() for i in range(self.size)]
 
     def add(self, key: int) -> None:
         bucket = self.buckets[self.hashFunc(key)]
